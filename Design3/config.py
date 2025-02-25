76ee727d74c36ca4f26d700c7278780a94548a97
Melissa Wu <mzwu173@gmail.com>
2025-02-25 00:24:28 -0500

    fix update/logging order, queue access

--- Design3/config.py
+++ Design3/config.py
@@ -1,5 +1,6 @@
 # config.py
 
 NUM_VIRTUAL_MACHINES = 3        # how many virtual machines are we going to simulate?
-PORT = 12321                     # what port should the machines all connect to to communicate?
+PORT = 12321                    # what port should the machines all connect to to communicate?
 LOG_DIR = "vm_logs/"            # directory for where the virtual machines' log files will go
+UPPER_BOUND = 10                # upper bound for random int generation in case of no message in queue - must be >=3, higher means higher probability of internal event
--- Design3/vm.py
+++ Design3/vm.py
@@ -28,15 +28,19 @@ class VirtualMachine(multiprocessing.Process):
         log_file        specify where all log output should go for this vm 
         """
         super().__init__()
+        
         # Create id, ports, and queue
         self.id = id
         self.port = config.PORT + id
         self.peer_ports = [(i, config.PORT + i) for i in range(config.NUM_VIRTUAL_MACHINES) if i != id]
         self.queue = multiprocessing.Queue()
-        self.queue_size = 0 # qsize() does not work on MacOS devices, so keep track separately
+        self.queue_size = multiprocessing.Value('i', 0) # qsize() does not work on MacOS devices, so keep track separately, need to use multiprocessing.Value to access from different processes
+
         # Create clock
         self.clock_speed = random.randint(1, 6) # randomize number of instructions/second
         self.logical_clock = 0
+        self.start_time = time.time()  # to standardize system time
+
         # Create and clear log file
         self.log_file = f"{config.LOG_DIR}vm_{id}.log"
         os.makedirs(config.LOG_DIR, exist_ok=True)
@@ -53,27 +57,35 @@ class VirtualMachine(multiprocessing.Process):
             except ConnectionRefusedError:
                 return False
             
-    def update(self, log_msg, sender_logical_clock=None):
+    def update(self, sender_logical_clock=None):
         """ Update using logical clock rules and log a message. """
         # Logical clock rules
         if sender_logical_clock is not None:
             self.logical_clock = max(self.logical_clock, sender_logical_clock) + 1
         else:
             self.logical_clock += 1
+
+    def log(self, msg):
         with open(self.log_file, "a") as f:
-            f.write(log_msg + "\n")
+            f.write(msg + "\n")
     
     def send_msg_and_update(self, list_of_recipients):
         """ Perform both sending a msg and updating for a list of recipient vms. """
         for recipient in list_of_recipients:
+
             # Create message (local logical clock time) and send it to the recipient's port
             recipient_vm_port = recipient[1]
             message = f"{self.id},{self.logical_clock}"
             status = self.send_msg(recipient_vm_port, message)
+
             # Update personal logical clock
-            log_success = f"[Sent Msg To {recipient[0]}, System Time {time.time()}, Logical Clock Time {self.logical_clock}] SUCCESS"
-            log_failure = f"[Sent Msg To {recipient[0]}, System Time {time.time()}, Logical Clock Time {self.logical_clock}] FAILURE"
-            self.update(log_success) if status else self.update(log_failure)   
+            self.update()
+            if status:
+                log_success = f"Sent Msg To {recipient[0]}\tSystem Time {time.time()-self.start_time:.3f}\tLogical Clock Time {self.logical_clock}\tSUCCESS"
+                self.log(log_success)
+            else:
+                log_failure = f"Sent Msg To {recipient[0]}\tSystem Time {time.time()-self.start_time:.3f}\tLogical Clock Time {self.logical_clock}\tFAILURE"
+                self.log(log_failure)
 
     def receive_msg(self):
         """ Listen for incoming/received messages and store them in a queue. """
@@ -88,7 +100,8 @@ class VirtualMachine(multiprocessing.Process):
                         sender_id, sender_logical_clock = map(int, data.decode("utf-8").split(","))
                         # Put tuple (sender_id, sender_logical_clock) in our own queue
                         self.queue.put((sender_id, sender_logical_clock))
-                        self.queue_size += 1
+                        with self.queue_size.get_lock():
+                            self.queue_size.value += 1
 
     def handle_queue(self):
         """ Take one message off queue, update local logical clock, and write to log. """
@@ -96,10 +109,13 @@ class VirtualMachine(multiprocessing.Process):
         # Timeout=1 in case queue becomes empty
         # As an added, safeguard we also check before even calling handle_queue()
         sender_id, sender_logical_clock = self.queue.get(timeout=1) 
-        self.queue_size -= 1
+        with self.queue_size.get_lock():
+            self.queue_size.value -= 1
+        
         # Update logical clock and write to log
-        log_msg = f"[Received Msg From {sender_id}, System Time {time.time()}, New Msg Queue Length {self.queue_size}, Logical Clock Time {self.logical_clock}]"
-        self.update(log_msg, sender_logical_clock)
+        self.update(sender_logical_clock)
+        log_msg = f"Received Msg From {sender_id}\tSystem Time {time.time()-self.start_time:.3f}\tNew Msg Queue Length {self.queue_size.value}\tLogical Clock Time {self.logical_clock}"
+        self.log(log_msg)
 
     def run(self):
         """ Main execution loop. """
@@ -115,22 +131,28 @@ class VirtualMachine(multiprocessing.Process):
                 # If queue is not empty, perform the actions required
                 if not self.queue.empty():
                     self.handle_queue()
+
                 # If queue is empty, generate a random value
                 else:
-                    value = random.randint(1, 10)
+                    value = random.randint(1, config.UPPER_BOUND)
+
                     # Send message to one machine
                     if value == 1:
                         self.send_msg_and_update([self.peer_ports[0]])
+
                     # Send message to other machines
                     elif value == 2:
                         self.send_msg_and_update([self.peer_ports[1]])
+
                     # Send message to both machines
                     elif value == 3:
                         self.send_msg_and_update(self.peer_ports)
+
                     # Internal event: simply increment logical clock and log info
                     else:
-                        log_msg = f"[Internal, System Time {time.time()}, Logical Clock Time {self.logical_clock}] success"
-                        self.update(log_msg)
+                        self.update()
+                        log_msg = f"Internal Event\tSystem Time {time.time()-self.start_time:.3f}\tLogical Clock Time {self.logical_clock}\tSUCCESS"
+                        self.log(log_msg)
 
         # Graceful termination
         except KeyboardInterrupt:
